71cae0d2f24c76007c9d3b695ffbc298eb0433d3
RokiLord <youht7290@gmail.com>
2022-04-27 18:57:22 +0900

    practice4

---
+++ hyuntaek/ch04/practice4/src/main.rs
@@ -0,0 +1,22 @@
+fn main(){
+    let s = String::from("Hello");
+
+    takes_ownership(s);
+
+    let x = 5;
+
+    makes_copy(x);
+
+    
+    println!("{}",x);
+
+
+}
+
+fn takes_ownership(some_string: String){
+    println!("{}",some_string);
+}
+
+fn makes_copy(some_integer: i32){
+    println!("{}",some_integer);
+}
