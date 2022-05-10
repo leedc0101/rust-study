fbbcbb5289dcf5ea497b7a2b82e6fed46b3af2a1
RokiLord <youht7290@gmail.com>
2022-05-10 20:36:28 +0900

    Zip

--- hyuntaek/ch08/src/main.rs
+++ hyuntaek/ch08/src/main.rs
@@ -1,7 +1,11 @@
 fn main(){
-    let hello = "helloworld";
-    let answer = &hello[0..1];
+    
 
-    answer + hello;
+    let teams = vec![String::from("Blue"), String::from("Yellow")];
+    let initial_scores = vec![10, 50];
+
+    let scores =
+        teams.into_iter().zip(initial_scores.into_iter());
+
+    println!("{:?}",scores);
 }
-
